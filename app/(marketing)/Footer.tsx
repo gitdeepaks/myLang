945cc50d2d07961c8cdc6379a31ec7cef28c0e33
gitdeepaks <deepaksankhyan92@outlook.com>
2024-03-06 19:38:02 +0530

    first commit 🚀

---
+++ app/(marketing)/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="hidden lg:block w-full h-20 border-t-2 border-slate-200 p-2">
+      Footer
+    </footer>
+  );
+};
