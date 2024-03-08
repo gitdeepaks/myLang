06bc96ed15ccf2aa8f3c7873e85cc8b58da7d9cc
gitdeepaks <deepaksankhyan92@outlook.com>
2024-03-08 16:35:10 +0530

    added learn page  🚀

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,25 @@
+import { FeedWrapper } from "@/components/FeedWrapper";
+import { StrickyWrapper } from "@/components/StrickyWrapper";
 import React from "react";
+import { HeaDer } from "./HeaDer";
+import { UserProgress } from "@/components/UserProgress";
 
 const LearnPage = () => {
-  return <div>LearnPage</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StrickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/ES - Spain.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSunscription={false}
+        />
+      </StrickyWrapper>
+      <FeedWrapper>
+        <HeaDer title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
